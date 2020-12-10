c86a4db6b040e4dbb55c8f79a057508e2cda6e4e
Andre Marcelino <marcelinoandre@gmail.com>
2020-12-10 17:17:18 -0300

    feat: setup - add:  configTypOrm and module products

---
+++ README.md
@@ -0,0 +1,32 @@
+
+
+# Crud básico produtos
+
+crud desenvolvido baseado nessa playlist:
+
+https://www.youtube.com/playlist?list=PL4bT56Uw3S4zIAOX1ddx8oJS6jaD43vZC
+
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  DB_SYNCRONIZE,
+} from './config/constants';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get(DB_HOST),
+        port: +configService.get<number>(DB_PORT),
+        username: configService.get(DB_USER),
+        password: configService.get(DB_PASSWORD),
+        database: configService.get(DB_DATABASE),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get(DB_SYNCRONIZE),
+      }),
+      inject: [ConfigService],
+    }),
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
